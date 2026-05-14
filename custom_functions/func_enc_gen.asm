e17d6ac4115bb7a81a78e2301f5243c6ccffae1f
JSsixtyfour <justinsynhorst@gmail.com>
2026-05-13 23:12:41 -0500

    Fixed and improved Random Trainer Selection
    In line with improved random pokemon selection

--- custom_functions/func_enc_gen.asm
+++ custom_functions/func_enc_gen.asm
@@ -66,18 +66,50 @@ GetRandMon:
 	push bc
     ld a, b
     cp a, $4
-    jr z, pokeball_class_selection_trainer
+    jr z, trainer_pokeball_class_selection
     cp a, $3
-    jr z, greatball_class_selection_trainer
+    jr z, trainer_greatball_class_selection
+    cp a, $2
+    jr z, trainer_ultraball_class_selection
+    cp a, $1
+    jr z, trainer_masterball_class_selection
     
 ; common
-pokeball_class_selection_trainer:
+
+trainer_pokeball_class_selection:
+ld hl, pokeball_class
+push hl
+ld a, pokeball_pokemon_line_amount
+push af
+jp pokemon_class_selection_trainer
+
+trainer_greatball_class_selection:
+ld hl, greatball_class
+push hl
+ld a, greatball_pokemon_line_amount
+push af
+jp pokemon_class_selection_trainer
+
+trainer_ultraball_class_selection:
+ld hl, ultraball_class
+push hl
+ld a, ultraball_pokemon_line_amount
+push af
+jp pokemon_class_selection_trainer
+
+trainer_masterball_class_selection:
+ld hl, masterball_class
+push hl
+ld a, masterball_pokemon_line_amount
+push af
+
+pokemon_class_selection_trainer:
 call Random                 ; get a random number to determine pokemon
 ldh [hMultiplicand+2], a    ; place number in for multiplication
 xor a
 ldh [hMultiplicand], a      ; put zero in highest byte
 ldh [hMultiplicand+1], a    ; put second byte for multiplication
-ld a, $1D                   ; multiply by amount of this class
+pop af                      ; restore line amount to multiply by amount in class
 ldh [hMultiplier], a        ; place amount of class in multiplier
 call Multiply               ; multiply random number by amount in class
 ldh   a, [hProduct+2]       ; load product into a
@@ -86,151 +118,23 @@ ldh   a, [hProduct+3]
 ldh [hDividend+1], a
 
 ld a, $FF                   ; load 255
-ld b, $2
+ld b, $2                    ; b determines how many bytes the number is, do not remove!
 ldh [hDivisor], a           ; place 255 as divisor
 call Divide
-ldh   a, [hQuotient+3]      ; load in quotient
-ldh [hMultiplicand], a      ; set quotient as multiplier
-ld a, $2
-ldh [hMultiplier], a        ; load 2, which is the size of each struct in array
-xor a
-ldh [hMultiplicand+1], a    ; clear out other digits
-ldh [hMultiplicand+2], a
-call Multiply               ; multiply result by size of struct to add to base address
-ld hl, hProduct+1           ; load pokemon pointer
-ld c,[hl]                   ; load offset to add to pointer, to get address
+ldh   a, [hQuotient+3]      ; load in quotient, which will be the offset
+ld c, a                     ; place in c
 ld b, $0
 
-ld hl, pokeball_class+2     ; load base pointer
+pop hl                      ; restore base pointer
 add hl, bc                  ; add product to get address of pokemon
+           
 ld a, [hl]                  ; load pokemon from address
 ld [wCurPartySpecies], a    ; place pokemon in Current Party Speciies
 
+.done
 pop bc
 pop hl
 RET
-
-; rare
-
-greatball_class_selection_trainer:
-call Random
-ldh [hMultiplicand+2], a
-xor a
-ldh [hMultiplicand], a
-ldh [hMultiplicand+1], a
-ld a, $1B
-ldh [hMultiplier], a
-call Multiply
-ldh   a, [hProduct+2]
-ldh [hDividend], a
-ldh   a, [hProduct+3]
-ldh [hDividend+1], a
-
-ld a, $FF
-ld b, $2
-ldh [hDivisor], a
-call Divide
-ldh   a, [hQuotient+3]
-ldh [hMultiplicand], a
-ld a, $2
-ldh [hMultiplier], a
-xor a
-ldh [hMultiplicand+1], a
-ldh [hMultiplicand+2], a
-call Multiply
-ld hl, hProduct+1
-ld c,[hl]
-ld b, $0
-
-ld hl, greatball_class+2
-add hl, bc                  ; add product to get address of pokemon
-ld a, [hl]                  ; load pokemon from address
-ld [wCurPartySpecies], a    ; place pokemon in Current Party Speciies
-pop bc
-pop hl
-RET
-;
-;ultraball_class_selection:
-;
-;call Random
-;ldh [hMultiplicand+2], a
-;xor a
-;ldh [hMultiplicand], a
-;ldh [hMultiplicand+1], a
-;ld a, $10
-;ldh [hMultiplier], a
-;call Multiply
-;ldh   a, [hProduct+2]
-;ldh [hDividend], a
-;ldh   a, [hProduct+3]
-;ldh [hDividend+1], a
-;
-;ld a, $FF
-;ld b, $2
-;ldh [hDivisor], a
-;call Divide
-;ldh   a, [hQuotient+3]
-;ldh [hMultiplicand], a
-;ld a, $2
-;ldh [hMultiplier], a
-;xor a
-;ldh [hMultiplicand+1], a
-;ldh [hMultiplicand+2], a
-;call Multiply
-;ld hl, hProduct+1
-;ld c,[hl]
-;ld b, $0
-;
-;ld hl, ultraball_class+1
-;add hl, bc
-;ld a, [hld]
-;cp b
-;jr z, ultraball_load
-;jp ultraball_class_selection
-;
-;
-;ultraball_load:
-;ld [hl], 0x1
-;inc [hl]
-;ld d, [hl]
-;
-;RET
-;
-;masterball_class_selection:
-;
-;rogue_pokemon_randomized_batch::
-;   call Random
-;   call Random_Pokemon_Selection
-;   ld hl, wRoguePokemon1
-;   ld [hl], d
-;   call Random
-;   call Random_Pokemon_Selection
-;   ld hl, wRoguePokemon2
-;   ld [hl], d
-;   call Random
-;   call Random_Pokemon_Selection
-;   ld hl, wRoguePokemon3
-;   ld [hl], d
-;   
-;RET
-;.loop
-;	ld a, b
-;	and a
-;	jr z, .endloop
-;	inc hl
-;	dec b
-;	ld a, [hl]
-;	and a
-;	jr nz, .loop
-;	ld h, d
-;	ld l, e
-;	jr .loop
-;.endloop
-;	ld a, [hl]
-	pop bc
-	pop hl
-	ld [wCurPartySpecies], a
-	ret
 	
 ;generates a randomized 6-party enemy trainer roster
 GetRandRoster:
